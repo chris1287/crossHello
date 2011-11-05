e25e4070686dd5c84c09a06dfb98fbd01f986509
Cristiano Prato <cristiano.prato@gmail.com>
2011-11-05 20:11:36 +0100

    Initial commit

---
+++ src/crossHello.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  crossHello.c
+ *
+ *    Description:  Test - Hello world cross-compiled for bot Linux and Windows
+ *
+ *        Version:  1.0
+ *        Created:  11/05/2011 01:56:28 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Cristiano Prato (CrP), cristiano.prato@gmail.com
+ *        Company:  Zirak Srl
+ *
+ * =====================================================================================
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int arch, char *argv[])
+{
+	printf ( "Cross hello!!!\n" );
+	
+#ifdef HOST_LINUX
+	printf ( "This is Linux version\n" );
+#endif
+#ifdef HOST_WINDOWS
+	printf ( "This is Windows version\n" );
+#endif
+
+	printf ( "End of program - exit now\n" );
+
+	return 0;
+}
